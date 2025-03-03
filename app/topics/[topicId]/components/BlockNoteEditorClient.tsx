fed437b8e1657eaea309e14ba69f25d8677caebb
Valentin349 <valentino7007@gmail.com>
2025-03-03 22:53:36 +0000

    remove media blocks and emojies from text editor

--- app/topics/[topicId]/components/BlockNoteEditorClient.tsx
+++ app/topics/[topicId]/components/BlockNoteEditorClient.tsx
@@ -1,10 +1,26 @@
 "use client"
 import { BlockNoteView } from "@blocknote/mantine";
-import { useCreateBlockNote } from "@blocknote/react";
+import { DefaultReactSuggestionItem, getDefaultReactSlashMenuItems, SuggestionMenuController, useCreateBlockNote } from "@blocknote/react";
 import "@blocknote/mantine/style.css";
+import { BlockNoteEditor, filterSuggestionItems } from "@blocknote/core";
+
+const getCustomSlashMenuItems = (editor: BlockNoteEditor): DefaultReactSuggestionItem[] => {
+    return getDefaultReactSlashMenuItems(editor).filter(
+        (item) => !["Image", "Video", "Table", "File", "Embed", 'Audio', 'Emoji'].includes(item.title) // Exclude media-related blocks
+    );
+};
 
 export default function BlockNoteEditorClient() {
     const editor = useCreateBlockNote({});
 
-    return <BlockNoteView editor={editor} />
+    return (
+        <BlockNoteView editor={editor} slashMenu={false} >
+            <SuggestionMenuController
+                triggerCharacter={"/"}
+                getItems={async (query) =>
+                    filterSuggestionItems(getCustomSlashMenuItems(editor), query)
+                  }
+            />
+        </BlockNoteView>
+    );
 }
