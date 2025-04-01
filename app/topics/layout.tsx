396b0ae1fd9803efce78c0e378e92fe5474966bc
Valentin349 <valentino7007@gmail.com>
2025-04-01 22:59:08 +0100

    Fix Topic page layout

--- app/topics/layout.tsx
+++ app/topics/layout.tsx
@@ -1,24 +1,24 @@
 import DynamicBreadcrumbs from "@/components/Breacrumbs";
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { Separator } from "@radix-ui/react-separator";
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";;
 
 export default function TopicsLayout({ children }: { children: React.ReactNode }) {
     return (
-        <SidebarProvider>
-            <div className="flex min-h-screen">
+        <>
+            <SidebarProvider>
                 <AppSidebar />
-                <SidebarInset>
+                <SidebarInset className="w-full">
                     <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
                         <div className="flex items-center gap-2 px-4">
                             <SidebarTrigger className="-ml-1" />
                             <Separator orientation="vertical" className="mr-2 h-4" />
-                            <DynamicBreadcrumbs/>
+                            <DynamicBreadcrumbs />
                         </div>
                     </header>
-                    <main className="flex p-6">{children}</main>
+                    <main className="px-4">{children}</main>
                 </SidebarInset>
-            </div>
-        </SidebarProvider>
+            </SidebarProvider>
+        </>
     );
 }
